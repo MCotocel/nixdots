6ac261cf50229674fa7b5937beac71a2ef56b993
MCotocel <MCotocel@outlook.com>
2021-07-02 15:28:47 +0400

    emacs

---
+++ config/emacs/etc/private.el
@@ -0,0 +1,4 @@
+(setq libera-password "890iopkl;,./")
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark)) 
+(setq frame-title-format "")
