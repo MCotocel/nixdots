fe3a92097b1cfbff2e8fb402f53dce9c58e24a93
Matei Cotocel <mcotocel@outlook.com>
2021-07-02 17:06:28 +0400

    awesome enabled

--- hosts/thebe/configuration.nix
+++ hosts/thebe/configuration.nix
@@ -64,12 +64,8 @@
     shell = pkgs.zsh;
   };
 
-  windowManager.awesome = {
+  services.xserver.windowManager.awesome = {
     enable = true;
-    luaModules = with pkgs.luaPackages; [
-     luarocks
-     awestore
-    ];
   };
 
   # System-wide packages
