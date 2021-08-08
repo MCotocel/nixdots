96c4e16f69a5f64a17a6af721c60cef7e11da218
Matei Cotocel <mcotocel@outlook.com>
2021-08-08 12:47:33 +0400

    update

--- hosts/ganymede/home.nix
+++ hosts/ganymede/home.nix
@@ -59,7 +59,7 @@
     ".config/mpd/mpd.conf".source         = ../../config/mpd/mpd.conf;
 
     # User binaries
-    #".bin/nas".source = ../../nas;
+    ".bin/nas".source = ../../nas;
     ".bin".source     = ../../config/bin;
   };
 }
