1b7c5f8184aca854153831754b7b4c2cf1488d4e
Matei Cotocel <mcotocel@outlook.com>
2021-08-10 10:40:45 +0400

    update

--- hosts/ganymede/configuration.nix
+++ hosts/ganymede/configuration.nix
@@ -322,7 +322,26 @@
   
   console.colors = [ "181e23" "ff8080" "97d59b" "fffe80" "80d1ff" "c780ff" "80ffe4" "d5d5d5" "ffaeae" "bef8c1" "fcfba6" "ace1ff" "d8a8ff" "a2ffeb" "ffffff" ]; # Color for the console
 
-  nixpkgs.overlays = [ ../../overlays/awesome.nix ];
+  nixpkgs.overlays = [
+    (self: super: {
+      final: prev: {
+        awesome = (prev.awesome.overrideAttrs (old: rec {
+          src = prev.fetchFromGitHub {
+            owner = "awesomeWM";
+            repo = "awesome";
+            rev = "149d18e0e796b3a439b1d79c5ee0c93febfcdf69";
+            sha256 = "02ahbph10sd5a4gv9wizcl0pmqd08mdc47w9bd28p5bldpk4vrvm";
+          };
+          GI_TYPELIB_PATH = "${prev.playerctl}/lib/girepository-1.0:"
+            + "${prev.upower}/lib/girepository-1.0:" + old.GI_TYPELIB_PATH;
+        })).override {
+          stdenv = prev.clangStdenv;
+          luaPackages = prev.lua52Packages;
+          gtk3Support = true;
+        };
+      }
+    })
+  ];
 
   nixpkgs.config = {
     allowUnfree = true; # Allow unfree packages (forgive me stallman)
