94a22cf8778bdb0e4317a68195f94c43d89a98a7
Matei Cotocel <mcotocel@outlook.com>
2021-08-10 10:35:07 +0400

    update

--- flake.nix
+++ flake.nix
@@ -8,38 +8,39 @@
     agenix.url       = "github:ryantm/agenix";
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, agenix }@inputs:
-    with nixpkgs.lib;
-    let
-      user-overlays = importNixFiles ./overlays;
-    in {
-      nixosConfigurations = {
-        thebe = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-	        home-manager.nixosModules.home-manager {
-              home-manager.users.matei = import ./hosts/thebe/home.nix;
-	        }
-	        { nixpkgs.overlays = [ nur.overlay ]; }
-	        ./hosts/thebe/configuration.nix
-          ];
-        };
-        ganymede = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            agenix.nixosModules.age
-	        home-manager.nixosModules.home-manager {
-              home-manager.users.matei = import ./hosts/ganymede/home.nix;
-	        }
-	        { nixpkgs.overlays = [
-              nur.overlay
-              (import (builtins.fetchTarball {
-                url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-              }))
-              ]; }
-	        ./hosts/ganymede/configuration.nix
-          ];
-        };
+  outputs = { self, nixpkgs, home-manager, nur, agenix }: {
+
+    nixosConfigurations = {
+
+      thebe = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+	      home-manager.nixosModules.home-manager {
+            home-manager.users.matei = import ./hosts/thebe/home.nix;
+	      }
+	      { nixpkgs.overlays = [ nur.overlay ]; }
+	      ./hosts/thebe/configuration.nix
+        ];
       };
+
+      ganymede = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          agenix.nixosModules.age
+	      home-manager.nixosModules.home-manager {
+            home-manager.users.matei = import ./hosts/ganymede/home.nix;
+	      }
+	      { nixpkgs.overlays = [
+            nur.overlay
+            (import (builtins.fetchTarball {
+              url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+            }))
+            ]; }
+	      ./hosts/ganymede/configuration.nix
+        ];
+      };
+
     };
+
+  };
 }
