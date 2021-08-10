179f365cef1e698c8d381e333fcd807bc31d8a9c
Matei Cotocel <mcotocel@outlook.com>
2021-08-10 11:14:07 +0400

    update

--- hosts/ganymede/configuration.nix
+++ hosts/ganymede/configuration.nix
@@ -326,7 +326,7 @@
     (self: super: {                                                                                                                                                                                                                                        
       awesome = super.awesome.overrideAttrs (oldAttrs: rec {
         src = builtins.fetchGit https://github.com/awesomewm/awesome;
-        rev = "a4572b9b52d89369ce3bd462904d536ec116dc35";
+        rev = "8a81745d4d0466c0d4b346762a80e4f566c83461";
         sha = "1kj2qz2ns0jn5gha4ryr8w8vvy23s3bb5z3vjhwwfnrv7ypb40iz";
         }
       );
