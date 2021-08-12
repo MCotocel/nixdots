7b53fa067246d36e470f4cfc8130cb6acea2304a
Matei Cotocel <mcotocel@outlook.com>
2021-08-12 13:01:08 +0400

    update

--- hosts/ganymede/hardware-configuration.nix
+++ hosts/ganymede/hardware-configuration.nix
@@ -3,14 +3,9 @@
 {
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "uas" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" "amdgpu" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-  boot.kernelParams= [
-    "iommu=soft"
-    "radeon.ci_support=0"
-    "amdgpu.si_support=1"
-    "amdgpu.dc=1
-  "];
+  boot.kernelParams = [ "iommu=soft" ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-label/nixos";
