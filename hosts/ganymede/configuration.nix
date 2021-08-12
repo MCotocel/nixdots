f45e8288aa868894322016fab3815950a6f7e886
Matei Cotocel <mcotocel@outlook.com>
2021-08-12 09:44:15 +0400

    update

--- hosts/ganymede/configuration.nix
+++ hosts/ganymede/configuration.nix
@@ -32,13 +32,6 @@
     layout = "us"; # Set keyboard layout
     autoRepeatDelay = 225; # Keyboard repeat rate
     autoRepeatInterval = 33;
-    videoDrivers = [
-      "amdgpu"
-      "radeon"
-      #"amdgpu-pro"
-      "fbdev"
-      "modesetting"
-    ];
   };
 
   # Sound
