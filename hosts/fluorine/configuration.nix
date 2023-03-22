b986f8572f47ac32ab9409156474c6b401c3aa84
Matei Cotocel <mcotocel@outlook.com>
2023-03-22 18:44:32 +0400

    Try adding new system

---
+++ hosts/fluorine/configuration.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware.nix
+      ./code.nix
+      ./services.nix
+      ./grafana.nix
+#      ./home.nix
+      ../shared.nix
+    ];
+
+  networking.hostName = "fluorine";
+  networking.networkmanager.enable = true;
+  networking.interfaces.wlan0.ipv4.addresses = [ {
+    address = "192.168.168.121";
+    prefixLength = 24;
+  } ];
+  networking.defaultGateway = "192.168.168.121";
+
+
+  users.users.matei = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ];
+     shell = pkgs.zsh;
+     password = "1234";
+   };
+
+  services.openssh.enable = true;
+
+  networking.firewall.enable = false;
+
+  system.stateVersion = "22.11";
+}
+
